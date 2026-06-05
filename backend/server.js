3e76161f0e3401fd9d0f6b167cb90385404e9914
MZ2026qut <monija.safi@connect.qut.edu.au>
2026-06-05 21:07:30 +1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
 // server.js
 
-// 1. Force Node.js to use Google's unfiltered DNS for database routing
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-
-// 2. Load environment variables
+// 1. Load environment variables
 const dotenv = require('dotenv');
 dotenv.config({ path: __dirname + '/.env' });
 
-// 3. Import remaining dependencies safely
+// 2. Import remaining dependencies safely
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -24,9 +20,9 @@ console.log("--- DEBUGGING ENV LOAD ---");
 console.log("Current Directory:", __dirname);
 console.log("Parsed Secret Key:", process.env.JWT_SECRET);
 
-// FIX: Run database and start server unconditionally so PM2 can bind to it
+// Run database and start server unconditionally
 connectDB();
-const PORT = process.env.PORT || 80; // Default straight to 80 for production deployment
+const PORT = process.env.PORT || 80; 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
